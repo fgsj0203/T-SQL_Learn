03a2b2a748e9801536df445ca10d653ea9cbb011
Francisco Junior <fgsj.dev@outlook.com>
2026-01-14 23:02:23 -0300

    update: inserting exercise

--- logical_operator_SQL_V1.0.sql
+++ logical_operator_SQL_V1.0.sql
@@ -16,3 +16,14 @@ SELECT *
 FROM SalesLT.[Product] as sp
 WHERE sp.Color = 'Red' OR sp.Color = 'Blue'
 ORDER BY sp.[Name] ASC
+
+-- Exercise 03: Return customers is have title 'Ms' and middlename 'Smith'
+SELECT *
+FROM SalesLT.Customer as sc
+WHERE sc.Title = 'Ms' AND sc.MiddleName = 'Smith'
+
+-- Exercise 04: return products with listPrice in range 100, 500 (using signal > and <)
+SELECT *
+FROM SalesLT.[Product] as sp
+WHERE sp.ListPrice > 100 AND sp.ListPrice < 500
+ORDER BY sp.[Name] ASC
