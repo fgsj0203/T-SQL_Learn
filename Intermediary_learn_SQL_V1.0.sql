27435b639e80f46691f601f48ba332038b2d366f
Francisco Junior <fgsj.dev@outlook.com>
2026-01-22 10:05:18 -0300

    update: first exercise

---
+++ Intermediary_learn_SQL_V1.0.sql
@@ -0,0 +1,9 @@
+/*
+Author: Francisco
+College: Universidade Estacio de Sa
+Course: Ciencia da Computacao
+Date: 22-01-2026, in format Brazil
+Database example: AdventureWorksLT2025
+*/
+
+-- Exercise 01: 
