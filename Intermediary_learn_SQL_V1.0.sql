30d190b0c04007ca2b2ebeba3cf0450601eec666
Francisco Junior <fgsj.dev@outlook.com>
2026-01-23 09:11:45 -0300

    update: new exercise

--- Intermediary_learn_SQL_V1.0.sql
+++ Intermediary_learn_SQL_V1.0.sql
@@ -6,4 +6,24 @@ Date: 22-01-2026, in format Brazil
 Database example: AdventureWorksLT2025
 */
 
--- Exercise 01: 
+-- Exercise 01: Liste todos os produtos com preco de tabela (ListPrice) superior a $1.000. / Return all producst with price bigger $1.000
+SELECT *
+FROM SalesLT.[Product] as sp
+WHERE sp.ListPrice > 1000
+
+-- Exercise 02: Recupere os nomes das empresas que possuem um vendedor (SalesPerson) especifico (ex: 'adventure-works\linda3'). / Return of names companies with have salesPerson specific (ex: 'adventure-works\linda3')
+SELECT sc.CompanyName as 'Nome da empresa'
+FROM SalesLT.Customer as sc
+WHERE sc.SalesPerson = 'adventure-works\linda3'
+
+-- Exercise 03: Selecione produtos que pesam mais de 1.000g e estao na cor 'Black'. / Return products of weight bigger 1000g and color is 'Black'
+SELECT *
+FROM SalesLT.[Product] as sp
+WHERE sp.[Weight] > 1000 AND sp.Color = 'Black'
+
+-- Exercise 04: Filtre clientes que nao possuem um numero de telefone registrado. / Return customer of have not number cellphone.
+SELECT *
+FROM SalesLT.Customer as sc
+WHERE sc.Phone IS NULL
+
+
